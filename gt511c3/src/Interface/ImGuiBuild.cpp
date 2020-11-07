80a6c71e736ca40c7e9940757a844c2e8e8739c2
Mel Florance <mel.florance@gmail.com>
2020-11-07 19:21:59 +0100

    Fix linux path for imgui impl

--- gt511c3/src/Interface/ImGuiBuild.cpp
+++ gt511c3/src/Interface/ImGuiBuild.cpp
@@ -1,3 +1,3 @@
 #define IMGUI_IMPL_OPENGL_LOADER_GLAD
-#include "examples/imgui_impl_opengl3.cpp"
-#include "examples/imgui_impl_glfw.cpp"
+#include <../vendor/imgui/examples/imgui_impl_opengl3.cpp>
+#include <../vendor/imgui/examples/imgui_impl_glfw.cpp>
