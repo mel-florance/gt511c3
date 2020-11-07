ca8cb6f9eb976a606c3363babc3c830e2ba615e7
Mel Florance <mel.florance@gmail.com>
2020-11-07 19:00:22 +0100

    Remove useless typedef

--- gt511c3/src/Core/Core.h
+++ gt511c3/src/Core/Core.h
@@ -11,12 +11,3 @@
 #define PI 3.141592653589793f
 #define TAU 2.0f * PI
 #define EPSILON 0.001f
-
-typedef __int16 int16;
-typedef unsigned __int16 uint16;
-
-typedef __int32 int32;
-typedef unsigned __int32 uint32;
-
-typedef __int64 int64;
-typedef unsigned __int64 uint64;
